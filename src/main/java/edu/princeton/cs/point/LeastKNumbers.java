e8930d8b0f2c60e022441eeb50e9ff130a4f686f
chengkai <lakers32@126.com>
2021-02-25 15:58:35 +0800

    answer of Point Offer with test

--- src/main/java/edu/princeton/cs/point/LeastKNumbers.java
+++ src/main/java/edu/princeton/cs/point/LeastKNumbers.java
@@ -1,8 +1,6 @@
 package edu.princeton.cs.point;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @description: 剑指 Offer 40. 最小的k个数（top K问题）
@@ -36,7 +34,7 @@ public class LeastKNumbers {
      * 保持堆的大小为K，然后遍历数组中的数字，遍历的时候做如下判断：
      * 1. 若目前堆的大小 小于K，将当前数字放入堆中。
      * 2. 否则判断当前数字与大根堆堆顶元素的大小关系，如果当前数字比大根堆堆顶还大，这个数就直接跳过；
-     *    反之如果当前数字比大根堆堆顶小，先poll掉堆顶，再将该数字放入堆中。
+     * 反之如果当前数字比大根堆堆顶小，先poll掉堆顶，再将该数字放入堆中。
      *
      * @param arr
      * @param k
@@ -52,7 +50,7 @@ public class LeastKNumbers {
             queue.offer(arr[i]);
         }
         for (int i = k; i < arr.length; ++i) {
-            // 最大堆的首个原始值最大
+            // 最大堆的首个值最大
             if (queue.peek() > arr[i]) {
                 queue.poll();
                 queue.offer(arr[i]);
@@ -136,9 +134,91 @@ public class LeastKNumbers {
         nums[j] = temp;
     }
 
+    /**
+     * 二叉搜索树
+     * 与前两种方法相比，BST 有一个好处是求得的前K大的数字是有序的
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public static int[] solution4(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // TreeMap的key是数字, value是该数字的个数。
+        // cnt表示当前map总共存了多少个数字。
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int cnt = 0;
+        for (int num : arr) {
+            // 1. 遍历数组，若当前map中的数字个数小于k，则map中当前数字对应个数+1
+            if (cnt < k) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                cnt++;
+                continue;
+            }
+            // 2. 否则，取出map中最大的Key（即最大的数字), 判断当前数字与map中最大数字的大小关系：
+            //    若当前数字比map中最大的数字还大，就直接忽略；
+            //    若当前数字比map中最大的数字小，则将当前数字加入map中，并将map中的最大数字的个数-1。
+            Map.Entry<Integer, Integer> entry = map.lastEntry();
+            if (entry.getKey() > num) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                // put之后，lastEntry的值已经改变，该值为刚刚put的值
+                if (entry.getValue() == 1) {
+                    map.pollLastEntry();
+                } else {
+                    map.put(entry.getKey(), entry.getValue() - 1);
+                }
+            }
+
+        }
+
+        // 最后返回map中的元素
+        int[] res = new int[k];
+        int idx = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int freq = entry.getValue();
+            while (freq-- > 0) {
+                res[idx++] = entry.getKey();
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 数据范围有限时直接计数排序
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public static int[] solution5(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // 统计每个数字出现的次数(num是索引)
+        int[] counter = new int[10001];
+        for (int num : arr) {
+            counter[num]++;
+        }
+        // 根据counter数组从头找出k个数作为返回结果
+        int[] res = new int[k];
+        int idx = 0;
+        for (int num = 0; num < counter.length; num++) {
+            while (counter[num]-- > 0 && idx < k) {
+                // num是数值
+                res[idx++] = num;
+            }
+            if (idx == k) {
+                break;
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr = {4, 5, 1, 6, 2, 7, 3, 8};
         int k = 4;
-        System.out.println("Least K nums are" + Arrays.toString(solution2(arr, k)));
+        System.out.println("Least K nums are" + Arrays.toString(solution5(arr, k)));
     }
 }
