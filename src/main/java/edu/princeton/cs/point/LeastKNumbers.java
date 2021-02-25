2919b0b8503e56da513b0db075ceb41963b1e496
chengkai <lakers32@126.com>
2021-02-25 13:53:37 +0800

    answer of Point Offer with test

---
+++ src/main/java/edu/princeton/cs/point/LeastKNumbers.java
@@ -0,0 +1,138 @@
+package edu.princeton.cs.point;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+/**
+ * @description: 剑指 Offer 40. 最小的k个数（top K问题）
+ * <p>
+ * 输入整数数组arr，找出其中最小的k个数。例如，输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
+ * <p>
+ * https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+ * @author: cheng kai
+ * @create: 2021-02-24 22:11
+ **/
+public class LeastKNumbers {
+
+    /**
+     * 排序后取出前k个数
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public static int[] solution(int[] arr, int k) {
+        int[] vec = new int[k];
+        Arrays.sort(arr);
+        for (int i = 0; i < k; ++i) {
+            vec[i] = arr[i];
+        }
+        return vec;
+    }
+
+    /**
+     * 最小堆
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public static int[] solution2(int[] arr, int k) {
+        int[] vec = new int[k];
+        if (k == 0) {
+            return vec;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>((num1, num2) -> num2 - num1);
+        for (int i = 0; i < k; ++i) {
+            queue.offer(arr[i]);
+        }
+        for (int i = k; i < arr.length; ++i) {
+            if (queue.peek() > arr[i]) {
+                queue.poll();
+                queue.offer(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = queue.poll();
+        }
+        return vec;
+    }
+
+    /**
+     * 快排
+     *
+     * @param arr
+     * @param k
+     * @return
+     */
+    public static int[] solution3(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // 最后一个参数表示我们要找的是下标为k-1的数
+        return quickSearch(arr, 0, arr.length - 1, k - 1);
+    }
+
+    /**
+     * 快速排序
+     *
+     * @param nums
+     * @param lo
+     * @param hi
+     * @param k
+     * @return
+     */
+    private static int[] quickSearch(int[] nums, int lo, int hi, int k) {
+        // 每快排切分1次，找到排序后下标为j的元素，如果j恰好等于k就返回j以及j左边所有的数；
+        int j = partition(nums, lo, hi);
+        if (j == k) {
+            return Arrays.copyOf(nums, j + 1);
+        }
+        // 否则根据下标j与k的大小关系来决定继续切分左段还是右段。
+        return j > k ? quickSearch(nums, lo, j - 1, k) : quickSearch(nums, j + 1, hi, k);
+    }
+
+    /**
+     * 快排切分，返回下标j，使得比nums[j]小的数都在j的左边，比nums[j]大的数都在j的右边。
+     *
+     * @param nums
+     * @param lo
+     * @param hi
+     * @return
+     */
+    private static int partition(int[] nums, int lo, int hi) {
+        int v = nums[lo];
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (++i <= hi && nums[i] < v) ;
+            while (--j >= lo && nums[j] > v) ;
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        nums[lo] = nums[j];
+        nums[j] = v;
+        return j;
+    }
+
+    /**
+     * 交换数组中的下标值
+     *
+     * @param nums
+     * @param i
+     * @param j
+     */
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 1, 6, 2, 7, 3, 8};
+        int k = 4;
+        System.out.println("Least K nums are" + Arrays.toString(solution3(arr, k)));
+    }
+}
