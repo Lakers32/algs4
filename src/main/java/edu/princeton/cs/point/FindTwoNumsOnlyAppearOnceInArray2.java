304d23b8595a7971b04284b25e135384f11d2470
chengkai <lakers32@126.com>
2021-03-04 16:12:10 +0800

    answer of Point Offer with test

--- src/main/java/edu/princeton/cs/point/FindTwoNumsOnlyAppearOnceInArray2.java
+++ src/main/java/edu/princeton/cs/point/FindTwoNumsOnlyAppearOnceInArray2.java
@@ -29,8 +29,31 @@ public class FindTwoNumsOnlyAppearOnceInArray2 {
         return ones;
     }
 
+    /**
+     * 遍历统计
+     *
+     * @param nums
+     * @return
+     */
+    public static int solution2(int[] nums) {
+        int[] counts = new int[32];
+        for (int num : nums) {
+            for (int j = 0; j < 32; j++) {
+                counts[j] += num & 1;
+                num >>>= 1;
+            }
+        }
+        int val = 0, m = 3;
+        for (int i = 0; i < 32; i++) {
+            val <<= 1;
+            val |= counts[31 - i] % m;
+        }
+
+        return val;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {9,1,7,9,7,9,7};
+        int[] nums = {9, 1, 7, 9, 7, 9, 7};
 
         System.out.println("The num only appear once is " + solution(nums));
     }
