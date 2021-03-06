efc41e58149c2643f4e4d9f8cf2d5f763d701a8f
chengkai <lakers32@126.com>
2021-03-06 11:11:29 +0800

    answer of Point Offer with test

---
+++ src/main/java/edu/princeton/cs/point/JosephCircle.java
@@ -0,0 +1,36 @@
+package edu.princeton.cs.point;
+
+/**
+ * @description: 剑指 Offer 62. 圆圈中最后剩下的数字
+ * <p>
+ * 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。求出这个圆圈里剩下的最后一个数字。
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof
+ * @author: cheng kai
+ * @create: 2021-03-06 11:07
+ **/
+public class JosephCircle {
+
+    /**
+     * 动态规划
+     *
+     * @param n
+     * @param m
+     * @return
+     */
+    public static int solution(int n, int m) {
+        int x = 0;
+
+        for (int i = 2; i <= n; i++) {
+            x = (x + m) % i;
+        }
+
+        return x;
+    }
+
+    public static void main(String[] args) {
+        int n = 5, m = 3;
+        System.out.println("The last remaining value is " + solution(n, m));
+    }
+}
